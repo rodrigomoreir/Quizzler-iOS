e9fcaf52059ae1eeb3776c91fe06c9e2475e9c56
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2021-08-26 08:10:02 -0300

    chore: code refactoring for the MVC

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle! //True, False
+//        let actualQuestion = quiz[questionNumber]
+//        let actualAnswer = actualQuestion.answer
+//        let actualAnswer = quiz[questionNumber].answer -> notação anterior a aplicação do MVC
+//        quizBrain.checkAnswer(userAnswer: userAnswer) -> forma de escrita com nome de parametro externo
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        
+//      userGotItRight ? sender.backgroundColor = UIColor.green : sender.backgroundColor = UIColor.red
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+//        updateUI()
+        
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+    }
+    
+}
+
--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -1,20 +1,14 @@
 //
-//  ViewController.swift
+//  QuizBrain.swift
 //  Quizzler-iOS13
 //
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Rodrigo Alves on 25/08/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
-    
+struct QuizBrain {
     let quiz = [
         Question(q: "A slug's blood is green.", a: "True"),
         Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
@@ -28,50 +22,35 @@ class ViewController: UIViewController {
         Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
         Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
         Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-
     ]
     
     var questionNumber = 0
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        updateUI()
-    }
-
-    @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
-        let userAnswer = sender.currentTitle //True, False
-//        let actualQuestion = quiz[questionNumber]
-//        let actualAnswer = actualQuestion.answer
-        let actualAnswer = quiz[questionNumber].answer
-        
-        
-        if userAnswer == actualAnswer {
-            sender.backgroundColor = UIColor.green
+    // temos praticamente 2 tipos de métodos (functions): o simples, que não altera nada sobre a estrutura, e o método mutante (mutating) que pod alterar o estado da estrutura (struct)
+    
+    func checkAnswer(_ userAnswer: String) -> Bool {
+        if userAnswer == quiz[questionNumber].answer {
+            return true
         } else {
-            sender.backgroundColor = UIColor.red
+            return false
         }
-        
+    }
+    
+    func getQuestionText() -> String {
+        return quiz[questionNumber].text
+    }
+    
+    func getProgress() -> Float {
+        let progress = Float(questionNumber) / Float(quiz.count)
+        return progress
+    }
+    
+    // se estamos criando um método que modifica uma das propriedades dentro da estrutura, temos que marcar esse método como mutante, como abaixo. Dessa forma o "self" se comportará como var, e não como "let" que seria o default
+    mutating func nextQuestion () {
         if questionNumber + 1 < quiz.count {
             questionNumber += 1
         } else {
             questionNumber = 0
         }
-        
-        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-        
-//        updateUI()
-        
     }
-    
-    @objc func updateUI() {
-        questionLabel.text = quiz[questionNumber].text
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
-        
-        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
-    }
-    
 }
-
